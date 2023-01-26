d81418395486dbfaf2a4a8cdfabcf0a04e3828d7
Jorge Mora <jorgemorah671@gmail.com>
2023-01-25 19:19:48 -0600

    Add Local Wallpaper

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -5,33 +5,33 @@ local gears = require("gears")
 
 local theme = {}
 
-theme.wallpaper 	= "~/Pictures/Wallpapers/Wall1.jpg"
+theme.wallpaper 	= gears.filesystem.get_configuration_dir() .. "/theme/Wallpaper.jpg"
 
 theme.font 			= "Chillax 11"
 theme.icon_font 	= "Font Awesome 6 Free solid 7"
 theme.icon_font_alt = "FiraCode Nerd Font Mono"
 
---   _____     __           
+--   _____     __
 --  / ___/__  / /__  _______
 -- / /__/ _ \/ / _ \/ __(_-<
--- \___/\___/_/\___/_/ /___/                         
+-- \___/\___/_/\___/_/ /___/
 
 -- Using mocha colorscheme from cattpuccin
 -- https://github.com/catppuccin/catppuccin
 
 theme.transparent   = "#00000000"
 
-theme.black   	    = "#45475A" 
-theme.altblack      = "#585B70" 
-theme.white   	    = "#BAC2DE" 
-theme.altwhite      = "#A6ADC8" 
+theme.black   	    = "#45475A"
+theme.altblack      = "#585B70"
+theme.white   	    = "#BAC2DE"
+theme.altwhite      = "#A6ADC8"
 
-theme.red     	    = "#F38BA8" 
-theme.green   	    = "#A6E3A1" 
-theme.yellow  	    = "#F9E2AF" 
-theme.blue    	    = "#89B4FA" 
-theme.magenta 	    = "#F5C2E7" 
-theme.cyan    	    = "#94E2D5" 
+theme.red     	    = "#F38BA8"
+theme.green   	    = "#A6E3A1"
+theme.yellow  	    = "#F9E2AF"
+theme.blue    	    = "#89B4FA"
+theme.magenta 	    = "#F5C2E7"
+theme.cyan    	    = "#94E2D5"
 
 theme.useless_gap   = dpi(5)
 
@@ -44,10 +44,10 @@ theme.tag_focus 	= "#8CAAEE"
 theme.tag_normal	= "#BAC2DE"
 theme.tag_urgent 	= "#F38BA8"
 
---  _      ___ __           
+--  _      ___ __
 -- | | /| / (_) /  ___ _____
 -- | |/ |/ / / _ \/ _ `/ __/
--- |__/|__/_/_.__/\_,_/_/                             
+-- |__/|__/_/_.__/\_,_/_/
 
 theme.wibar_height 	= dpi(30)
 theme.wibar_font   	= "Chillax medium 12"
