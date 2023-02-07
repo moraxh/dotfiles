b12236049624157cdea65baa9c75f2b712cf346a
Jorge Mora <jorgemorah671@gmail.com>
2023-02-07 13:43:56 -0600

    Update dashboard conf

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,40 +1,52 @@
 local cmd = vim.cmd
 
-local db = require('dashboard')
+require('dashboard').setup({
+    theme = 'doom',
+    hide = { statusline, tabline },
+    disable_move = true,
+    config = {
+        header = {
+            "                        ",
+            "                        ",
+            "                        ",
+            "                        ",
+            " ▄▄▄██▀▀▀    ███▄ ▄███▓ ",
+            "   ▒██      ▓██▒▀█▀ ██▒ ",
+            "   ░██      ▓██    ▓██░ ",
+            "▓██▄██▓     ▒██    ▒██  ",
+            " ▓███▒      ▒██▒   ░██▒ ",
+            " ▒▓▒▒░      ░ ▒░   ░  ░ ",
+            " ▒ ░▒░      ░  ░      ░ ",
+            " ░ ░ ░      ░      ░    ",
+            " ░   ░             ░    ",
+            "                        ",
+            "                        ",
+            "                        ",
+            "                        ",
+        },
+        center = {
+            {icon = '        ', desc = 'New File      ', action = 'DashboardNewFile'},
+            {icon = '        ', desc = 'Last Session  ', action = 'silent source ~/.config/nvim/cache/Session.vim'},
+            {icon = '        ', desc = 'Find File     ', action = 'Telescope find_files cwd=~/ hidden=true'},
+            {icon = '        ', desc = 'Recent Files  ', action = 'Telescope oldfiles'},
+            {icon = '        ', desc = 'File Explorer ', action = 'NvimTreeToggle'},
+            {icon = '        ', desc = 'Edit Config   ', action = 'Telescope find_files cwd=~/.config hidden=true'},
+        },
+        footer = {
+            "",
+            "",
+            "",
+            "",
+            "The best time to plant a tree was 20 years ago. The second best time is now."
+        },
+    },
+})
 
-db.disable_statusline = 1
-db.header_pad = 1
-db.center_pad = 3
-db.footer_pad = 5
-
-db.custom_header = {
-    "                        ",
-    " ▄▄▄██▀▀▀    ███▄ ▄███▓ ",
-    "   ▒██      ▓██▒▀█▀ ██▒ ",
-    "   ░██      ▓██    ▓██░ ",
-    "▓██▄██▓     ▒██    ▒██  ",
-    " ▓███▒      ▒██▒   ░██▒ ",
-    " ▒▓▒▒░      ░ ▒░   ░  ░ ",
-    " ▒ ░▒░      ░  ░      ░ ",
-    " ░ ░ ░      ░      ░    ",
-    " ░   ░             ░    ",
-    "                        ",
-}
-
-db.custom_footer = { 'The best time to plant a tree was 20 years ago. The second best time is now.'}
-
-db.custom_center= {
-    {icon = '        ', desc = 'New File      ', action = 'DashboardNewFile'},
-    {icon = '        ', desc = 'Last Session  ', action = 'silent source ~/.config/nvim/cache/Session.vim'},
-    {icon = '        ', desc = 'Find File     ', action = 'Telescope find_files cwd=~/ hidden=true'},
-    {icon = '        ', desc = 'Recent Files  ', action = 'Telescope oldfiles'},
-    {icon = '        ', desc = 'File Explorer ', action = 'NvimTreeToggle'},
-    {icon = '        ', desc = 'Edit Config   ', action = 'Telescope find_files cwd=~/.config hidden=true'},
-}
-
-cmd([[highlight dashboardFooter    guifg=#89ddff]])
-cmd([[highlight dashboardHeader    guifg=#c3e88d]])
-cmd([[highlight dashboardCenter    guifg=#82aaff]])
+cmd([[highlight DashboardFooter     guifg=#89ddff]])
+cmd([[highlight DashboardHeader     guifg=#c3e88d]])
+cmd([[highlight DashboardDesc       guifg=#82aaff]])
+cmd([[highlight DashboardKey        guifg=#82aaff]])
+cmd([[highlight DashboardIcon       guifg=#82aaff]])
 
 
 -- Disable statusline on dashboard
