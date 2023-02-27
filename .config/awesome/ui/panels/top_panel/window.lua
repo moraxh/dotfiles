11e138fef95815876f5bace708c2b493283ff636
Jorge Mora <jorgemorah671@gmail.com>
2023-02-27 13:54:23 -0600

    No title if there's no clients

--- .config/awesome/ui/panels/top_panel/window.lua
+++ .config/awesome/ui/panels/top_panel/window.lua
@@ -1,18 +1,31 @@
 local utilities = require("utilities")
 
+local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 
-return function(s) 
+return function(s)
 
-	widget = wibox.widget({	
+	widget = wibox.widget({
 		widget = wibox.widget.textbox,
 		font = beautiful.wibar_font,
 	})
 
-	client.connect_signal("focus", function() 
+	client.connect_signal("focus", function()
 		widget.markup = utilities.ui.format(tostring(client.focus.class):gsub("^%l", string.upper), beautiful.white, false, nil)
 	end)
+
+    client.connect_signal("request::unmanage", function()
+        if #(awful.screen.focused().selected_tag:clients()) == 0 then
+		    widget.markup = utilities.ui.format("", beautiful.white, false, nil)
+        end
+    end)
+
+    tag.connect_signal("property::selected", function()
+        if #(awful.screen.focused().selected_tag:clients()) == 0 then
+		    widget.markup = utilities.ui.format("", beautiful.white, false, nil)
+        end
+    end)
 
 	return widget
 end
